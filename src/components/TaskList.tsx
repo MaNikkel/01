430e8b6e6dffb2321d656115d0572e2bf5e2b4ca
Junior Rios <junioorrioos06@gmail.com>
2021-03-14 22:41:24 -0300

    Salvando alterações nas actions do template

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,17 +14,33 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
-  function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-  }
+    function getRandom() {
+        return Math.random();
+    }
 
-  function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-  }
+    function handleCreateNewTask() {
+        if (!newTaskTitle) {
+            return false;
+        }
 
-  function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
-  }
+        setTasks([...tasks, {
+            id: getRandom(),
+            title: newTaskTitle,
+            isComplete: false
+        }])
+    }
+
+    function handleToggleTaskCompletion(id: number) {
+        let tasksState = tasks;
+        tasksState = tasksState.map(task => task.id === id ? {...task, isComplete: !task.isComplete} : task);
+        setTasks(tasksState);
+    }
+
+    function handleRemoveTask(id: number) {
+        let tasksState = tasks;
+        tasksState = tasksState.filter(task => task.id !== id);
+        setTasks(tasksState);
+    }
 
   return (
     <section className="task-list container">
